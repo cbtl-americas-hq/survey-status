4272be93dbec555e8e2f5b3371beb3a3f9720f12
cbtl-americas-hq <136393184+cbtl-americas-hq@users.noreply.github.com>
2023-11-06 12:19:26 -0800

    Update survey_status_1.js
    reseda

--- data/survey_status_1.js
+++ data/survey_status_1.js
@@ -1213,7 +1213,7 @@ var json_survey_status_1 = {
             "properties": {
                 "id": "81",
                 "StoreID": "115",
-                "surveystat": "pending"
+                "surveystat": "complete"
             },
             "geometry": {
                 "type": "Point",
