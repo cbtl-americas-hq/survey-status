6a9f7b432159cd66cfb16e51a4a3d9d3dbb05996
cbtl-americas-hq <136393184+cbtl-americas-hq@users.noreply.github.com>
2023-11-07 13:42:45 -0800

    Update survey_status_1.js
    El Toro Rd Cafe

--- data/survey_status_1.js
+++ data/survey_status_1.js
@@ -58,7 +58,7 @@ var json_survey_status_1 = {
             "properties": {
                 "id": "4",
                 "StoreID": "399",
-                "surveystat": "pending"
+                "surveystat": "complete"
             },
             "geometry": {
                 "type": "Point",
