9ca297091cde9b60b1a8599f0db34b4a09200864
cbtl-americas-hq <136393184+cbtl-americas-hq@users.noreply.github.com>
2023-11-09 14:39:44 -0800

    Update survey_status_1.js
    la palma

--- data/survey_status_1.js
+++ data/survey_status_1.js
@@ -463,7 +463,7 @@ var json_survey_status_1 = {
             "properties": {
                 "id": "31",
                 "StoreID": "331",
-                "surveystat": "pending"
+                "surveystat": "complete"
             },
             "geometry": {
                 "type": "Point",
