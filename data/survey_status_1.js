75b092895b45aca0541d5ab86f0459bfbe455e7f
cbtl-americas-hq <136393184+cbtl-americas-hq@users.noreply.github.com>
2023-11-09 15:11:53 -0800

    Update survey_status_1.js
    redondo beach dt

--- data/survey_status_1.js
+++ data/survey_status_1.js
@@ -223,7 +223,7 @@ var json_survey_status_1 = {
             "properties": {
                 "id": "15",
                 "StoreID": "377",
-                "surveystat": "pending"
+                "surveystat": "complete"
             },
             "geometry": {
                 "type": "Point",
