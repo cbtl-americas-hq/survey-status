695e44a5330d8573e92703cc53a3beb7d6087496
cbtl-americas-hq <136393184+cbtl-americas-hq@users.noreply.github.com>
2023-11-09 15:38:13 -0800

    Update survey_status_1.js
    manhattan village

--- data/survey_status_1.js
+++ data/survey_status_1.js
@@ -1618,7 +1618,7 @@ var json_survey_status_1 = {
             "properties": {
                 "id": "108",
                 "StoreID": "22",
-                "surveystat": "pending"
+                "surveystat": "complete"
             },
             "geometry": {
                 "type": "Point",
