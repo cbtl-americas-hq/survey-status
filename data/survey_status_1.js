c99a9ba945c6b9c1340d04fd16102319b131ff07
cbtl-americas-hq <136393184+cbtl-americas-hq@users.noreply.github.com>
2023-11-09 23:14:10 -0800

    Update survey_status_1.js
    brentwood

--- data/survey_status_1.js
+++ data/survey_status_1.js
@@ -1664,7 +1664,7 @@ var json_survey_status_1 = {
             "properties": {
                 "id": "111",
                 "StoreID": "2",
-                "surveystat": "pending"
+                "surveystat": "complete"
             },
             "geometry": {
                 "type": "Point",
