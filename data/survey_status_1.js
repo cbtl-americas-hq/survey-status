a32ddbe5d218018e6c63239e101fbd94d743408b
cbtl-americas-hq <136393184+cbtl-americas-hq@users.noreply.github.com>
2023-11-10 15:55:20 -0800

    Update survey_status_1.js
    santa clarita

--- data/survey_status_1.js
+++ data/survey_status_1.js
@@ -268,7 +268,7 @@ var json_survey_status_1 = {
             "properties": {
                 "id": "18",
                 "StoreID": "369",
-                "surveystat": "pending"
+                "surveystat": "complete"
             },
             "geometry": {
                 "type": "Point",
