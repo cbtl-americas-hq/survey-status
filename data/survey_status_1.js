65e62824ddb99e341860a7672d0f748bbbe7beda
cbtl-americas-hq <136393184+cbtl-americas-hq@users.noreply.github.com>
2023-11-10 13:26:55 -0800

    Update survey_status_1.js
    diamond bar

--- data/survey_status_1.js
+++ data/survey_status_1.js
@@ -208,7 +208,7 @@ var json_survey_status_1 = {
             "properties": {
                 "id": "14",
                 "StoreID": "378",
-                "surveystat": "pending"
+                "surveystat": "complete"
             },
             "geometry": {
                 "type": "Point",
