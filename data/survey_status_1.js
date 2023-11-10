1b495d5919999447a3ac9989c9bbe86c26e92376
cbtl-americas-hq <136393184+cbtl-americas-hq@users.noreply.github.com>
2023-11-10 01:06:52 -0800

    Update survey_status_1.js
    burbank

--- data/survey_status_1.js
+++ data/survey_status_1.js
@@ -1604,7 +1604,7 @@ var json_survey_status_1 = {
             "properties": {
                 "id": "107",
                 "StoreID": "29",
-                "surveystat": "pending"
+                "surveystat": "complete"
             },
             "geometry": {
                 "type": "Point",
