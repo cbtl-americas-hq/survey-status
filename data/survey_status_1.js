56713012ff9ab15492e265bd230bff5f6463728e
cbtl-americas-hq <136393184+cbtl-americas-hq@users.noreply.github.com>
2023-11-10 14:22:13 -0800

    Update survey_status_1.js
    glendora

--- data/survey_status_1.js
+++ data/survey_status_1.js
@@ -238,7 +238,7 @@ var json_survey_status_1 = {
             "properties": {
                 "id": "16",
                 "StoreID": "376",
-                "surveystat": "pending"
+                "surveystat": "complete"
             },
             "geometry": {
                 "type": "Point",
